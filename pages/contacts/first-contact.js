470178dab9e50c9424ed621ac7f1e6703c5aaffa
Shaneen Vitug <shaneenvitug@gmail.com>
2020-06-02 13:23:36 +1000

    first commit

---
+++ pages/contacts/first-contact.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function FirstContact() {
+  return (
+    <>
+      <h1>First Contact</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to Home</a>
+        </Link>
+      </h2>
+    </>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <>
+      <div>Home page coming soon!</div>
+      <p>Check out <Link href="/contacts/first-contact"><a>this page!</a></Link></p>
+    </>
+  )
+}
